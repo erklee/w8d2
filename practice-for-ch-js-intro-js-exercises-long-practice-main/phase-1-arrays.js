419d631532e3e2b06e1b122ce059350340336574
Eric Lee <erlee92@yahoo.com>
2023-12-06 12:07:15 -0500

    problem set

--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-1-arrays.js
@@ -1,1 +1,39 @@
-// Your code here
+Array.prototype.uniq = function(){
+    let arr = [];
+    this.forEach(function(ele){
+        if (!arr.includes(ele)) {
+            arr.push(ele)
+        }
+    }
+    )
+    return arr
+}
+// console.log([1,2,2,3,3,3].uniq())
+
+Array.prototype.twoSum = function(){
+    let arr = [];
+    for (let i = 0; i < this.length - 1; i++ ) {
+        for (let j = i + 1; j < this.length; j++) {
+            if (this[i] + this[j] === 0) 
+            arr.push([i, j]);
+        }
+    }
+    return arr
+}
+
+// console.log([-1, 0, 2, -2, 1].twoSum())
+
+Array.prototype.transpose = function(){
+    let arr = [];
+    for(let row = 0; row < this[0].length; row++) {
+        let newArr = [];
+        for (col = 0; col < this.length; col++) {
+            newArr.push(this[col][row])
+        }
+        arr.push(newArr)
+    }
+    return arr
+}
+
+console.log([[0, 1, 2], [3, 4, 5], [6, 7, 8]].transpose())
+
--- practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
+++ practice-for-ch-js-intro-js-exercises-long-practice-main/phase-2-enumerable.js
@@ -1,1 +1,41 @@
 // Your code here
+Array.prototype.myEach = function (callback) {
+    for (let i = 0; i < this.length; i++) {
+      callback(this[i]);
+    }
+
+  };
+  function callback(ele) {
+    console.log((ele + "!"));
+}
+  ["a", "b","c"].myEach(ele => (callback(ele)))
+
+Array.prototype.myMap = function(callback) {
+    arr = []
+    for (i = 0; i < this.length; i++) {
+        arr.push(callback(this[i]));
+    }
+    return arr;
+
+
+}
+function callback2(ele) {
+    return (ele*2);
+}
+
+console.log([1,2,3,4,5].myMap(callback2))
+
+
+
+Array.prototype.myReduce = function (callback, acc) {
+    const array = this.slice();
+    if (typeof acc === 'undefined') {
+      acc = array.shift();
+    }
+  
+    array.myEach(el => {
+      acc = callback(acc, el);
+    });
+  
+    return acc;
+  };
